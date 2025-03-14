9434568bbd4f4314b69fb1cc63d7d32a8ea8693f
Sascha Kehrli <skehrli@ethz.ch>
2025-03-14 14:05:29 +0100

    add tests and coverage

---
+++ tests/simulation_test.py
@@ -0,0 +1,15 @@
+from lantern.main import run_simulation
+from lantern.main import Season
+import random
+import pytest
+
+
+@pytest.mark.parametrize('iteration', range(20))
+def test_simulation(iteration):
+    random.seed(42)
+    community_size: int = random.randint(5, 100)
+    season: Season = random.choice(list(Season))
+    pv: int = random.randint(0, 100)
+    sd: int = random.randint(0, 100)
+    battery: bool = random.choice([True, False])
+    run_simulation(community_size, season.value, pv, sd, battery)
